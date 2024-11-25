bb3ed397cf81e3b9d919288af79dbcca5f430831
nidhish_t <nidhish_t@dell.com>
2024-11-25 19:06:11 +0530

    cleaned the content

--- sample-7-string-case.py
+++ sample-7-string-case.py
@@ -7,11 +7,3 @@ print(message1.lower()) # output: hello world!
 message2 = "HELLO"
 print(message2.isupper())   # output: True
 print(message2.islower())   # output: False
-
-
-print("this is a string".startswith("this")) # output: True
-print("this is a string".startswith("This")) # output: False
-
-
-print("this is a string".endswith("string")) # output: True
-print("this is a string".endswith("g")) # output: True
