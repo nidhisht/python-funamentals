c3e5309eb1e10030366cf6dbb75c5c63b03ed6c5
nidhish_t <nidhish_t@dell.com>
2024-11-25 13:05:52 +0530

    refactored the filename

---
+++ sample-2-assignment-operator.py
@@ -0,0 +1,38 @@
+num = 5
+
+# Addition
+a = 10
+print (a, "+", num )
+a += num
+print ("Result: ", a) # Output: 15
+
+# Subtraction
+s = 10
+print (s, "-", num)
+s -= num
+print ("Subtraction: ",s) # Output: 5
+
+# Division
+d = 10
+print (d, "/", num)
+d /= num
+print ("Division: ",d) # Output: 2
+
+# Multiplication
+m = 10
+print (m, "*", num)
+m *= num
+print ("Multiplicaton: ",m) # Output: 50
+
+
+# Exponentiation
+e = 10
+print (e, "**", num)
+e **= num
+print ("Exponentiation: ",e) # Output: 100000
+
+# Floor division
+fd = 10
+print (fd, "//", num)
+fd //= num
+print ("Floor Division: ",fd) # Output: 2
