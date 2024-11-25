c6581ddccf6bcbb523d01791d7d00cb38eb8ba65
nidhish_t <nidhish_t@dell.com>
2024-11-25 15:13:53 +0530

    Added string-case.py

---
+++ sample-7-string-case.py
@@ -0,0 +1,9 @@
+message1 = "Hello World!"
+
+print(message1.upper()) # output: HELLO WORLD!
+print(message1.lower()) # output: hello world!
+
+
+message2 = "HELLO"
+print(message2.isupper())   # output: True
+print(message2.islower())   # output: False
