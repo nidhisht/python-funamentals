d54727b075c8be8808fe3289ad8ec75c92458f9a
nidhish_t <nidhish_t@dell.com>
2024-11-26 13:59:07 +0530

    added loops

---
+++ sample-20-loop-while.py
@@ -0,0 +1,7 @@
+counter = 0
+
+while counter < 3:
+    print("counter {}".format(counter))
+    counter +=1
+
+print("exited while loop!")
---
+++ sample-22-loop-range.py
@@ -0,0 +1,21 @@
+print("-----------------")
+numberRange = range(10)
+print(type(numberRange))
+
+for number in numberRange:
+    print(number)
+
+print("-----------------")
+otherRange = range(5,10)
+for number in otherRange:
+    print(number)
+
+print("-----------------")
+sequenceRange = range (0,20,2)
+for number in sequenceRange:
+    print(number)
+
+print("-----------------")
+sequenceRange = range (20,1,-2)
+for number in sequenceRange:
+    print(number)
