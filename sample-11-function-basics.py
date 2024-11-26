34d7558ac3ebfe9d3a4d6cf714983c377adc6eef
nidhish_t <nidhish_t@dell.com>
2024-11-26 12:57:16 +0530

    updated function

--- sample-11-function-basics.py
+++ sample-11-function-basics.py
@@ -3,10 +3,11 @@ def greeting():
     print("welcome to the function!!")
     print("--------------------------")
 
-
+# Function accepting parameters
 def add(num1, num2):
     print(num1 + num2)
 
+# Function accepting default parameter values
 def sub(num1=10, num2=5):
     print(num1-num2)
 
